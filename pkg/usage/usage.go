ceab7065d64c444baa5f0cc4d03fa34efe55c00f
Xiaofei Du <xiaofei.du@instill.tech>
2022-08-21 18:46:23 +0100

    refactor: update usage reporter
    - move usage related code into usage package
    - trigger reporter before shutting down server

--- pkg/usage/usage.go
+++ pkg/usage/usage.go
@@ -3,31 +3,54 @@ package usage
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/instill-ai/connector-backend/config"
 	"github.com/instill-ai/connector-backend/internal/logger"
 	"github.com/instill-ai/connector-backend/pkg/datamodel"
 	"github.com/instill-ai/connector-backend/pkg/repository"
+	"github.com/instill-ai/x/repo"
 
 	connectorPB "github.com/instill-ai/protogen-go/vdp/connector/v1alpha"
 	mgmtPB "github.com/instill-ai/protogen-go/vdp/mgmt/v1alpha"
 	usagePB "github.com/instill-ai/protogen-go/vdp/usage/v1alpha"
+	usageClient "github.com/instill-ai/usage-client/client"
+	usageReporter "github.com/instill-ai/usage-client/reporter"
 )
 
 // Usage interface
 type Usage interface {
 	RetrieveUsageData() interface{}
+	StartReporter(ctx context.Context)
+	TriggerSingleReporter(ctx context.Context)
 }
 
 type usage struct {
 	repository        repository.Repository
 	userServiceClient mgmtPB.UserServiceClient
+	reporter          usageReporter.Reporter
+	version           string
 }
 
 // NewUsage initiates a usage instance
-func NewUsage(r repository.Repository, mu mgmtPB.UserServiceClient) Usage {
+func NewUsage(ctx context.Context, r repository.Repository, mu mgmtPB.UserServiceClient, usc usagePB.UsageServiceClient) Usage {
+	logger, _ := logger.GetZapLogger()
+
+	version, err := repo.ReadReleaseManifest("release-please/manifest.json")
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	reporter, err := usageClient.InitReporter(ctx, usc, usagePB.Session_SERVICE_MGMT, config.Config.Server.Edition, version)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
 	return &usage{
 		repository:        r,
 		userServiceClient: mu,
+		reporter:          reporter,
+		version:           version,
 	}
 }
 
@@ -166,5 +189,31 @@ func (u *usage) RetrieveUsageData() interface{} {
 		ConnectorUsageData: &usagePB.ConnectorUsageData{
 			Usages: pbConnectorUsageData,
 		},
+	}
+}
+
+func (u *usage) StartReporter(ctx context.Context) {
+	if u.reporter == nil {
+		return
+	}
+
+	logger, _ := logger.GetZapLogger()
+	go func() {
+		time.Sleep(5 * time.Second)
+		err := usageClient.StartReporter(ctx, u.reporter, usagePB.Session_SERVICE_MGMT, config.Config.Server.Edition, u.version, u.RetrieveUsageData)
+		if err != nil {
+			logger.Error(fmt.Sprintf("unable to start reporter: %v\n", err))
+		}
+	}()
+}
+
+func (u *usage) TriggerSingleReporter(ctx context.Context) {
+	if u.reporter == nil {
+		return
+	}
+	logger, _ := logger.GetZapLogger()
+	err := usageClient.SingleReporter(ctx, u.reporter, usagePB.Session_SERVICE_MGMT, config.Config.Server.Edition, u.version, u.RetrieveUsageData())
+	if err != nil {
+		logger.Fatal(err.Error())
 	}
 }
