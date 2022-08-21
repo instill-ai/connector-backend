8fe1376299b042e94c9e1c3cf39934902d94c491
Xiaofei Du <xiaofei.du@instill.tech>
2022-08-21 20:02:44 +0100

    refactor: update usage reporter (#35)
    Because
    
    - upgrade usage collection
    
    This commit
    
    - move usage related code into the usage package
    - trigger the reporter before shutting down the server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -10,7 +10,6 @@ import (
 	"regexp"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/rs/cors"
@@ -34,14 +33,10 @@ import (
 	"github.com/instill-ai/connector-backend/pkg/repository"
 	"github.com/instill-ai/connector-backend/pkg/service"
 	"github.com/instill-ai/connector-backend/pkg/usage"
-	"github.com/instill-ai/x/repo"
 	"github.com/instill-ai/x/zapadapter"
 
 	database "github.com/instill-ai/connector-backend/internal/db"
 	connectorPB "github.com/instill-ai/protogen-go/vdp/connector/v1alpha"
-	mgmtPB "github.com/instill-ai/protogen-go/vdp/mgmt/v1alpha"
-	usagePB "github.com/instill-ai/protogen-go/vdp/usage/v1alpha"
-	usageclient "github.com/instill-ai/usage-client/client"
 )
 
 func grpcHandlerFunc(grpcServer *grpc.Server, gwHandler http.Handler, CORSOrigins []string) http.Handler {
@@ -61,25 +56,6 @@ func grpcHandlerFunc(grpcServer *grpc.Server, gwHandler http.Handler, CORSOrigin
 			})),
 		&http2.Server{},
 	)
-}
-
-func startReporter(ctx context.Context, usageServiceClient usagePB.UsageServiceClient, r repository.Repository, mu mgmtPB.UserServiceClient) {
-
-	logger, _ := logger.GetZapLogger()
-
-	version, err := repo.ReadReleaseManifest("release-please/manifest.json")
-	if err != nil {
-		logger.Fatal(err.Error())
-	}
-
-	go func() {
-		time.Sleep(5 * time.Second)
-		usg := usage.NewUsage(r, mu)
-		err = usageclient.StartReporter(ctx, usageServiceClient, usagePB.Session_SERVICE_CONNECTOR, config.Config.Server.Edition, version, usg.RetrieveUsageData)
-		if err != nil {
-			logger.Error(fmt.Sprintf("unable to start reporter: %v\n", err))
-		}
-	}()
 }
 
 func main() {
@@ -189,10 +165,12 @@ func main() {
 	defer cancel()
 
 	// Start usage reporter
+	var usg usage.Usage
 	if !config.Config.Server.DisableUsage {
 		usageServiceClient, usageServiceClientConn := external.InitUsageServiceClient()
 		defer usageServiceClientConn.Close()
-		startReporter(ctx, usageServiceClient, repository, userServiceClient)
+		usg = usage.NewUsage(ctx, repository, userServiceClient, usageServiceClient)
+		usg.StartReporter(ctx)
 	}
 
 	var dialOpts []grpc.DialOption
@@ -238,6 +216,9 @@ func main() {
 	case err := <-errSig:
 		logger.Error(fmt.Sprintf("Fatal error: %v\n", err))
 	case <-quitSig:
+		if !config.Config.Server.DisableUsage {
+			usg.TriggerSingleReporter(ctx)
+		}
 		logger.Info("Shutting down server...")
 		grpcS.GracefulStop()
 	}
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.0
 	github.com/iancoleman/strcase v0.2.0
 	github.com/instill-ai/protogen-go v0.3.1-alpha
-	github.com/instill-ai/usage-client v0.1.1-alpha
+	github.com/instill-ai/usage-client v0.1.2-alpha
 	github.com/instill-ai/x v0.1.0-alpha.0.20220706215306-bceeac65f523
 	github.com/jackc/pgconn v1.12.1
 	github.com/knadh/koanf v1.4.2
