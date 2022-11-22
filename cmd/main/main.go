b9d882147ccb2456a09efd50ad8b21c706158b6d
Phelan164 <phelan.nguyen@instill.tech>
2022-11-22 16:05:12 +0700

    chore: resilient to non-running usage service

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -170,7 +170,10 @@ func main() {
 		usageServiceClient, usageServiceClientConn := external.InitUsageServiceClient()
 		defer usageServiceClientConn.Close()
 		usg = usage.NewUsage(ctx, repository, userServiceClient, usageServiceClient)
-		usg.StartReporter(ctx)
+		if usg != nil {
+			usg.StartReporter(ctx)
+		}
+
 	}
 
 	var dialOpts []grpc.DialOption
@@ -216,7 +219,7 @@ func main() {
 	case err := <-errSig:
 		logger.Error(fmt.Sprintf("Fatal error: %v\n", err))
 	case <-quitSig:
-		if !config.Config.Server.DisableUsage {
+		if !config.Config.Server.DisableUsage && usg != nil {
 			usg.TriggerSingleReporter(ctx)
 		}
 		logger.Info("Shutting down server...")
