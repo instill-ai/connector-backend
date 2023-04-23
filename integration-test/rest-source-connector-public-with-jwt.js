dee40dafebc65c0001c95e715f9f38e22d631f38
Xiaofei Du <xiaofei.du@instill.tech>
2023-04-23 11:29:35 +0100

    chore: return 404 for non-exist user and 401 for non-existent headers (#85)
    Because
    
    - currently, if a user does not exist in mgmt-backend, returns 500 status error
    
    This commit
    
    - return 404 when a user does not exist
    - return 401 when both `jwt-sub` and `owner-id` does not exist in the header 
    - update tests

--- integration-test/rest-source-connector-public-with-jwt.js
+++ integration-test/rest-source-connector-public-with-jwt.js
@@ -32,14 +32,14 @@ export function CheckCreate() {
         check(http.request("POST",
             `${connectorPublicHost}/v1alpha/source-connectors`,
             JSON.stringify(httpSrcConnector), constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors response status for HTTP source connector is 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors response status for HTTP source connector is 404`]: (r) => r.status === 404,
         });
 
         // Cannot create grpc source connector of a non-exist user
         check(http.request("POST",
             `${connectorPublicHost}/v1alpha/source-connectors`,
             JSON.stringify(gRPCSrcConnector), constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors response status for gRPC source connector is 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors response status for gRPC source connector is 404`]: (r) => r.status === 404,
         });
     });
 }
@@ -50,7 +50,7 @@ export function CheckList() {
 
         // Cannot list source connector of a non-exist user
         check(http.request("GET", `${connectorPublicHost}/v1alpha/source-connectors`, null, constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] GET /v1alpha/source-connectors response status is 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] GET /v1alpha/source-connectors response status is 404`]: (r) => r.status === 404,
         });
     });
 }
@@ -72,7 +72,7 @@ export function CheckGet() {
 
         // Cannot get a source connector of a non-exist user
         check(http.request("GET", `${connectorPublicHost}/v1alpha/source-connectors/${resHTTP.json().source_connector.id}`, null, constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] GET /v1alpha/source-connectors/${resHTTP.json().source_connector.id} response status is 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] GET /v1alpha/source-connectors/${resHTTP.json().source_connector.id} response status is 404`]: (r) => r.status === 404,
         });
 
         check(http.request("DELETE", `${connectorPublicHost}/v1alpha/source-connectors/${resHTTP.json().source_connector.id}`), {
@@ -108,7 +108,7 @@ export function CheckUpdate() {
             "PATCH",
             `${connectorPublicHost}/v1alpha/source-connectors/${gRPCSrcConnector.id}`,
             JSON.stringify(gRPCSrcConnector), constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] PATCH /v1alpha/source-connectors/${gRPCSrcConnector.id} response status for updating gRPC source connector 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] PATCH /v1alpha/source-connectors/${gRPCSrcConnector.id} response status for updating gRPC source connector 404`]: (r) => r.status === 404,
         });
 
         check(http.request("DELETE", `${connectorPublicHost}/v1alpha/source-connectors/${gRPCSrcConnector.id}`), {
@@ -125,12 +125,12 @@ export function CheckDelete() {
 
         // Cannot delete source connector of a non-exist user
         check(http.request("DELETE", `${connectorPublicHost}/v1alpha/source-connectors/source-http`, null, constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] DELETE /v1alpha/source-connectors/source-http response status 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] DELETE /v1alpha/source-connectors/source-http response status 404`]: (r) => r.status === 404,
         });
 
         // Cannot delete destination connector of a non-exist user
         check(http.request("DELETE", `${connectorPublicHost}/v1alpha/destination-connectors/destination-http`, null, constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] DELETE /v1alpha/destination-connectors/destination-http response status 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] DELETE /v1alpha/destination-connectors/destination-http response status 404`]: (r) => r.status === 404,
         });
     });
 }
@@ -152,7 +152,7 @@ export function CheckLookUp() {
 
         // Cannot look up source connector of a non-exist user
         check(http.request("GET", `${connectorPublicHost}/v1alpha/source-connectors/${resHTTP.json().source_connector.uid}/lookUp`, null, constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] GET /v1alpha/source-connectors/${resHTTP.json().source_connector.uid}/lookUp response status 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] GET /v1alpha/source-connectors/${resHTTP.json().source_connector.uid}/lookUp response status 404`]: (r) => r.status === 404,
         });
 
         check(http.request("DELETE", `${connectorPublicHost}/v1alpha/source-connectors/${resHTTP.json().source_connector.id}`), {
@@ -178,12 +178,12 @@ export function CheckState() {
 
         // Cannot connect source connector of a non-exist user
         check(http.request("POST", `${connectorPublicHost}/v1alpha/source-connectors/${resHTTP.json().source_connector.id}/connect`, null, constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors/${resHTTP.json().source_connector.id}/connect response status 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors/${resHTTP.json().source_connector.id}/connect response status 404`]: (r) => r.status === 404,
         });
 
         // Cannot disconnect source connector of a non-exist user
         check(http.request("POST", `${connectorPublicHost}/v1alpha/source-connectors/${resHTTP.json().source_connector.id}/disconnect`, null, constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors/${resHTTP.json().source_connector.id}/disconnect response status 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors/${resHTTP.json().source_connector.id}/disconnect response status 404`]: (r) => r.status === 404,
         });
 
         check(http.request("DELETE", `${connectorPublicHost}/v1alpha/source-connectors/${resHTTP.json().source_connector.id}`), {
@@ -213,7 +213,7 @@ export function CheckRename() {
             JSON.stringify({
                 "new_source_connector_id": "some-id-not-http"
             }), constant.paramsHTTPWithJwt), {
-            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors/${resHTTP.json().source_connector.id}/rename response status 500`]: (r) => r.status === 500,
+            [`[with random "jwt-sub" header] POST /v1alpha/source-connectors/${resHTTP.json().source_connector.id}/rename response status 404`]: (r) => r.status === 404,
         });
 
         check(http.request("DELETE", `${connectorPublicHost}/v1alpha/source-connectors/${httpSrcConnector.id}`), {
