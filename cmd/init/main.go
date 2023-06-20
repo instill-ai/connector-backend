37b18575b50c5bc03c7730301c044cd892c37690
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-06-20 10:05:29 +0800

    chore: only preDownloadImage if the image is needed

--- cmd/init/main.go
+++ cmd/init/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"go.opentelemetry.io/otel"
 
+	"github.com/gofrs/uuid"
 	"github.com/instill-ai/connector-backend/config"
+	"github.com/instill-ai/connector-backend/pkg/datamodel"
 	"github.com/instill-ai/connector-backend/pkg/logger"
+	"github.com/instill-ai/connector-backend/pkg/repository"
 
+	database "github.com/instill-ai/connector-backend/pkg/db"
 	connectorDestinationAirbyte "github.com/instill-ai/connector-destination/pkg/airbyte"
+	connectorPB "github.com/instill-ai/protogen-go/vdp/connector/v1alpha"
 )
 
 func main() {
 
+	if err := config.Init(); err != nil {
+		log.Fatal(err.Error())
+	}
 	ctx, cancel := context.WithCancel(context.Background())
 	ctx, span := otel.Tracer("init-tracer").Start(ctx,
 		"main",
@@ -22,6 +31,11 @@ func main() {
 
 	logger, _ := logger.GetZapLogger(ctx)
 
+	db := database.GetConnection()
+	defer database.Close(db)
+
+	repository := repository.NewRepository(db)
+
 	airbyte := connectorDestinationAirbyte.Init(logger, connectorDestinationAirbyte.ConnectorOptions{
 		MountSourceVDP:     config.Config.Container.MountSource.VDP,
 		MountTargetVDP:     config.Config.Container.MountTarget.VDP,
@@ -30,7 +44,21 @@ func main() {
 		VDPProtocolPath:    "/etc/vdp/vdp_protocol.yaml",
 	})
 
-	err := airbyte.(*connectorDestinationAirbyte.Connector).PreDownloadImage(logger)
+	conns, _, _, err := repository.ListConnectorsAdmin(ctx, datamodel.ConnectorType(connectorPB.ConnectorType_CONNECTOR_TYPE_DESTINATION), 1000, "", false)
+	if err != nil {
+		panic(err)
+	}
+
+	airbyteConnector := airbyte.(*connectorDestinationAirbyte.Connector)
+	var uids []uuid.UUID
+	for idx := range conns {
+		uid := conns[idx].ConnectorDefinitionUID
+		if airbyteConnector.HasUid(uid) {
+			uids = append(uids, uid)
+		}
+	}
+
+	err = airbyteConnector.PreDownloadImage(logger, uids)
 
 	if err != nil {
 		panic(err)
