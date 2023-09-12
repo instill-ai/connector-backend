fd01713f8bde756ee8922fa7c156543691c47260
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-09-12 18:43:45 +0800

    chore: update integration test to support consolidated gateway

--- integration-test/const.js
+++ integration-test/const.js
@@ -4,11 +4,7 @@ let proto
 let pHost, cHost, mHost
 let cPrivatePort, cPublicPort, pPublicPort, mPublicPort
 
-if (__ENV.API_GATEWAY_VDP_HOST && !__ENV.API_GATEWAY_VDP_PORT || !__ENV.API_GATEWAY_VDP_HOST && __ENV.API_GATEWAY_VDP_PORT) {
-  fail("both API_GATEWAY_VDP_HOST and API_GATEWAY_VDP_PORT should be properly configured.")
-}
-
-export const apiGatewayMode = (__ENV.API_GATEWAY_VDP_HOST && __ENV.API_GATEWAY_VDP_PORT);
+export const apiGatewayMode = (__ENV.API_GATEWAY_URL && true);
 
 if (__ENV.API_GATEWAY_PROTOCOL) {
   if (__ENV.API_GATEWAY_PROTOCOL !== "http" && __ENV.API_GATEWAY_PROTOCOL != "https") {
@@ -19,33 +15,25 @@ if (__ENV.API_GATEWAY_PROTOCOL) {
   proto = "http"
 }
 
-if (apiGatewayMode) {
-  // api gateway mode
-  pHost = cHost = __ENV.API_GATEWAY_VDP_HOST
-  cPrivatePort = 3082
-  pPublicPort = cPublicPort = __ENV.API_GATEWAY_VDP_PORT
-
-  // TODO: remove model-backend dependency
-  mHost = "api-gateway-model"
-  mPublicPort = 9080
+if (__ENV.API_GATEWAY_PROTOCOL) {
+  if (__ENV.API_GATEWAY_PROTOCOL !== "http" && __ENV.API_GATEWAY_PROTOCOL != "https") {
+    fail("only allow `http` or `https` for API_GATEWAY_PROTOCOL")
+  }
+  proto = __ENV.API_GATEWAY_PROTOCOL
 } else {
-  // direct microservice mode
-  pHost = "pipeline-backend"
-  cHost = "connector-backend"
-  mHost = "model-backend"
-  cPrivatePort = 3082
-  cPublicPort = 8082
-  pPublicPort = 8081
-  mPublicPort = 8083
+  proto = "http"
 }
 
-export const connectorPrivateHost = `${proto}://${cHost}:${cPrivatePort}`;
-export const connectorPublicHost = `${proto}://${cHost}:${cPublicPort}`;
-export const connectorGRPCPrivateHost = `${cHost}:${cPrivatePort}`;
-export const connectorGRPCPublicHost = `${cHost}:${cPublicPort}`;
-export const pipelinePublicHost = `${proto}://${pHost}:${pPublicPort}`;
-export const pipelineGRPCPublicHost = `${pHost}:${pPublicPort}`;
-export const modelPublicHost = `${proto}://${mHost}:${mPublicPort}`;
+
+export const pipelinePublicHost = apiGatewayMode ? `${proto}://${__ENV.API_GATEWAY_URL}/vdp` : `http://pipeline-backend:8081`
+export const pipelineGRPCPublicHost = `${__ENV.API_GATEWAY_URL}`;
+export const connectorPrivateHost = `http://connector-backend:3082`;
+export const connectorPublicHost = apiGatewayMode ? `${proto}://${__ENV.API_GATEWAY_URL}/vdp` : `http://connector-backend:8082`
+export const connectorGRPCPrivateHost = `connector-backend:3082`;
+export const connectorGRPCPublicHost = `${__ENV.API_GATEWAY_URL}`;
+export const mgmtPublicHost = apiGatewayMode ? `${proto}://${__ENV.API_GATEWAY_URL}/base` : `http://mgmt-backend:8084`
+
+
 
 export const csvDstDefRscName = "connector-definitions/airbyte-destination-csv"
 export const csvDstDefRscPermalink = "connector-definitions/8be1cf83-fde1-477f-a4ad-318d23c9f3c6"
